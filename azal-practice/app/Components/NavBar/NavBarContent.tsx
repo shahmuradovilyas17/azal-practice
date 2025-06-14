afa6fde4533ad0d4e9af02e9b6ac47222ce2b197
Ilyas Shahmuradov <ilyas.shahmuradov@mail.ru>
2025-06-15 03:16:15 +0400

    redesigning project

--- azal-practice/app/Components/NavBar/NavBarContent.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function NavBarContent() {
-  return (
-    <div className="box-border p-6 bg-white rounded-b-xl rounded-tr-xl">
-      <div className="!text-black">IN DEVELOPMENT</div>
-    </div>
-  );
-}
--- azal-practice/app/page.tsx
+++ azal-practice/app/page.tsx
@@ -1,13 +1,11 @@
 import { Header } from "./Components/Header";
-import { NavBar } from "./Components/NavBar/NavBar";
 import "./globals.css";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-[#01357e] pb-8">
+      <div className="pb-8">
         <Header />
-        <NavBar />
       </div>
     </>
   );
