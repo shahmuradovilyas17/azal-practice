5776dbd720f37ce0db5666b02891beb3e8bbdf03
Ilyas Shahmuradov <ilyas.shahmuradov@mail.ru>
2025-06-18 08:35:12 +0400

    sadasdasdasd

--- app/Components/Tickets/PopularDestinations/PopularDestinationItems.tsx
+++ app/Components/Tickets/PopularDestinations/PopularDestinationItems.tsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "./swiperButtons.css";
-import Image from "next/image";
+// import Image from "next/image";
 
 // import china from "../../../../public/images/china.avif";
 // import georgia from "../../../../public/images/georgia.avif";
